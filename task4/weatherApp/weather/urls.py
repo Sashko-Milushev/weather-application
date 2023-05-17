e170e75e0d1e97ce2fc97441fe4e73314b70fef2
Sashko Milushev <milushev.sashko@gmail.com>
2023-05-17 16:49:34 +0300

    Add '|' sign to separate coldest city and avg temperature in task3

--- task4/weatherApp/weather/urls.py
+++ task4/weatherApp/weather/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, search
 
-urlpatterns = (
+urlpatterns = [
     path('', home, name='home'),
-)
+    path('search/', search, name='search'),
+]
